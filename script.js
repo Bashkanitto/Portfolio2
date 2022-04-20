7137f78964f59411b50e6c15b7f958f8108c58f6
bashkanitto <aidyn-18@mail.ru>
2022-04-20 12:27:27 +0600

    portfolio2

---
+++ script.js
@@ -0,0 +1,44 @@
+let a = document.querySelector(".active");
+let p = document.querySelector(".passive");
+let p1 = document.querySelector(".passive1");
+let p2 = document.querySelector(".passive2");
+let p3 = document.querySelector(".passive3");
+
+function changer(){
+a.className = 'active'
+p.className = 'passive'
+p1.className = 'passive'
+p2.className = 'passive'
+p3.className = 'passive'
+}
+function changer2(){
+    a.className = 'passive'
+    p.className = 'active'
+    p1.className = 'passive'
+    p2.className = 'passive'
+    p3.className = 'passive'
+}
+function changer3(){
+    a.className = 'passive'
+    p.className = 'passive'
+    p1.className = 'active'
+    p2.className = 'passive'
+    p3.className = 'passive'
+
+}
+function changer4(){
+    a.className = 'passive'
+    p.className = 'passive'
+    p1.className = 'passive'
+    p2.className = 'active'
+    p3.className = 'passive'
+
+}
+function changer5(){
+    a.className = 'passive'
+    p.className = 'passive'
+    p1.className = 'passive'
+    p2.className = 'passive'
+    p3.className = 'active'
+
+}
